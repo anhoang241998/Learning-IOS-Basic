1ab7dfc20a2f51c7a45e0bcc42d0ad8aed1fd4d7
Nguyen Hoang An <an2419998@gmail.com>
2022-06-02 09:43:41 +0700

    learning auto layout with storyboard

--- IOS Foundations with UIKit/ViewController.swift
+++ IOS Foundations with UIKit/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let DEBUG_TAG = "⚠️"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        debugPrint(DEBUG_TAG, 123123)
     }
-
-
 }
-
