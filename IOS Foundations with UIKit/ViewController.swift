40e1f923bd3780f817fbcd9662233486ddf2386d
Nguyen Hoang An <an2419998@gmail.com>
2022-06-09 11:05:19 +0700

    adding button click function

--- IOS Foundations with UIKit/ViewController.swift
+++ IOS Foundations with UIKit/ViewController.swift
@@ -10,22 +10,82 @@ import UIKit
 class ViewController: UIViewController {
     private let DEBUG_TAG = "⚠️"
     
+    var buttonAndCardStackView = UIStackView()
+    var logo = UIImageView()
+    var backgroundImg = UIImageView()
+    var button = UIButton()
+    var cardStackView = UIStackView()
+    var firstCard = UIImageView()
+    var secondCard = UIImageView()
+    var playersStackView = UIStackView()
+    var playerLabel = UILabel()
+    var playerResultLabel = UILabel()
+    var cpuStackView = UIStackView()
+    var cpuLabel = UILabel()
+    var cpuResultLabel = UILabel()
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
+        
+        styles()
+        layout()
+        
+        button.addTarget(self, action: #selector(buttonClicked), for: .primaryActionTriggered)
     }
     
-    private func setupViews() {
-        let backgroundImg = makeImageView(withImage: #imageLiteral(resourceName: "background"))
-        let logo = makeImageView(withImage: #imageLiteral(resourceName: "logo"))
+    @objc func buttonClicked() {
         
-        let button = makeButton(withText: "", withImage: #imageLiteral(resourceName: "dealbutton"))
+        // Randomize some numbers
+        let randomLeftNumber = Int.random(in: 2...14)
+        let randomRightNumber = Int.random(in: 2...14)
         
-        let buttonAndCardStackView = makeStackView(distribution: .fill, direction: .vertical)
-        let cardStackView = makeStackView(distribution: .fill, direction: .horizontal)
-        let firstCard = makeImageView(withImage: #imageLiteral(resourceName: "card6"))
-        let secondCard = makeImageView(withImage: #imageLiteral(resourceName: "card10"))
+        // Update the image views
+        firstCard.image = UIImage(named: "card\(randomLeftNumber)")
+        secondCard.image = UIImage(named: "card\(randomRightNumber)")
         
+        // Compare the random numbers
+        if randomLeftNumber > randomRightNumber {
+            // Left side wins
+            leftScore += 1
+            playerResultLabel.text = "\(leftScore)"
+        } else if randomLeftNumber < randomRightNumber {
+            // Right side wins
+            rightScore += 1
+            cpuResultLabel.text = "\(rightScore)"
+        } else {
+            // Tie
+        }
+    }
+    
+}
+
+extension ViewController {
+    
+    private func styles() {
+        buttonAndCardStackView = makeStackView(distribution: .fill, direction: .vertical)
+        
+        backgroundImg = makeImageView(withImage: #imageLiteral(resourceName: "background"))
+        
+        logo = makeImageView(withImage: #imageLiteral(resourceName: "logo"))
+        button = makeButton(withText: "", withImage: #imageLiteral(resourceName: "dealbutton"))
+        
+        cardStackView = makeStackView(distribution: .fill, direction: .horizontal)
+        firstCard = makeImageView(withImage: #imageLiteral(resourceName: "card6"))
+        secondCard = makeImageView(withImage: #imageLiteral(resourceName: "card10"))
+        
+        playersStackView = makeStackView(distribution: .fill, direction: .vertical)
+        playerLabel = makeLabel(withText: "Player", fontSize: 17)
+        playerResultLabel = makeLabel(withText: "0", fontSize: 40)
+        
+        cpuStackView = makeStackView(distribution: .fill, direction: .vertical)
+        cpuLabel = makeLabel(withText: "CPU", fontSize: 17)
+        cpuResultLabel = makeLabel(withText: "0", fontSize: 40)
+    }
+    
+    private func layout() {
         cardStackView.addArrangedSubview(firstCard)
         cardStackView.addArrangedSubview(secondCard)
         cardStackView.spacing = 30
@@ -34,17 +94,9 @@ class ViewController: UIViewController {
         buttonAndCardStackView.addArrangedSubview(button)
         buttonAndCardStackView.spacing = 30
         
-        let playersStackView = makeStackView(distribution: .fill, direction: .vertical)
-        let playerLabel = makeLabel(withText: "Player", fontSize: 17)
-        let playerResultLabel = makeLabel(withText: "0", fontSize: 40)
-        
         playersStackView.addArrangedSubview(playerLabel)
         playersStackView.addArrangedSubview(playerResultLabel)
         playersStackView.alignment = .leading
-        
-        let cpuStackView = makeStackView(distribution: .fill, direction: .vertical)
-        let cpuLabel = makeLabel(withText: "CPU", fontSize: 17)
-        let cpuResultLabel = makeLabel(withText: "0", fontSize: 40)
         
         cpuStackView.addArrangedSubview(cpuLabel)
         cpuStackView.addArrangedSubview(cpuResultLabel)
@@ -101,6 +153,38 @@ class ViewController: UIViewController {
         }
     }
     
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        let width = self.view.traitCollection.horizontalSizeClass
+        let height = self.view.traitCollection.verticalSizeClass
+        
+        if width == .compact && height == .compact {
+            buttonAndCardStackView.spacing = 20
+
+            logo.contentMode = .scaleAspectFit
+            NSLayoutConstraint.activate([
+                logo.heightAnchor.constraint(equalToConstant: 55)
+            ])
+
+            NSLayoutConstraint.activate([
+                buttonAndCardStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 30)
+            ])
+
+        } else if width == .compact && height == .regular {
+            buttonAndCardStackView.spacing = 30
+            
+            NSLayoutConstraint.activate([
+                buttonAndCardStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 20)
+            ])
+            
+            NSLayoutConstraint.activate([
+                logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+                logo.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+        }
+        
+    }
+    
     private func makeLabel(withText text: String, fontSize: CGFloat) -> UILabel {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -128,5 +212,4 @@ class ViewController: UIViewController {
         
         return button
     }
-    
 }
